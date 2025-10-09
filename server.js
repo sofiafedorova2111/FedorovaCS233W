f8aae930198e34bfe0d5b01ada78876d22853890
sofiafedorova2111 <fedorova.sofia@student.clackamas.edu>
2025-10-08 17:39:08 -0700

    server.js

--- server.js
+++ server.js
@@ -5,9 +5,12 @@ const PORT = 3000;
 const AppName = 'WeBuy';
 
 
-app.get('/' , (req, res) =>
+app.get('/' , (req, res) => {
 
-res.send("<h1> WeBuy </h1> "));
+const dateNow = new Date();
+
+ res.send(`<h1> WeBuy </h1> <p>Current time: ${dateNow}</p>`)
+});
 
 app.get('/about' , (req, res) => {
     res.send(
