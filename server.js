32e4ca6f63e401eeda72290571b5dccf18babe1e
sofiafedorova2111 <fedorova.sofia@student.clackamas.edu>
2025-10-15 19:05:46 -0700

    all changes

--- server.js
+++ server.js
@@ -1,28 +1,27 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-
 const AppName = 'WeBuy';
 
+app.use(express.json());
 
-app.get('/' , (req, res) => {
-
-const dateNow = new Date();
-
- res.send(`<h1> WeBuy </h1> <p>Current time: ${dateNow}</p>`)
+app.get('/', (req, res) => {
+  const dateNow = new Date();
+  res.send(`<h1>WeBuy</h1><p>Current time: ${dateNow}</p>`);
 });
 
-app.get('/about' , (req, res) => {
-    res.send(
-        `<h1> ${AppName} </h1>
-        <h1> Sofia Fedorova</h1>
-        <h1> Fall Term 2025 </h1>`
-        )
+app.get('/about', (req, res) => {
+  res.send(`
+    <h1>${AppName}</h1>
+    <h1>Sofia Fedorova</h1>
+    <h1>Fall Term 2025</h1>
+  `);
+});
 
-}) 
-
-
+const listRoutes = require('./routes/lists');
+app.use('/list', listRoutes);
 
 app.listen(PORT, () => {
-    console.log('Web server currently listening on port');
-})
+  console.log(`Web server currently listening on port ${PORT}`);
+});
+
